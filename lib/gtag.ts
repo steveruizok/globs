2157ae0e9e6ca503caccbca8649fd822db07bf78
Steve Ruiz <steveruizok@gmail.com>
2021-04-10 22:30:21 +0100

    Fixes typings

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.GA_MEASUREMENT_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID, {
+  ;(window as any).gtag("config", GA_TRACKING_ID, {
     page_path: url,
   })
 }
@@ -16,7 +16,7 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag("event", action, {
+  ;(window as any).gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
