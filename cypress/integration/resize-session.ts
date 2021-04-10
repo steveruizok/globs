a48f6b00c33a4cfc84979497d3d011a6f8ee5233
Steve Ruiz <steveruizok@gmail.com>
2021-04-10 23:16:50 +0100

    double fixes typings

--- cypress/integration/resize-session.ts
+++ cypress/integration/resize-session.ts
@@ -1,6 +1,5 @@
 import { IData } from "types"
 import inputs from "lib/inputs"
-import * as vec from "lib/vec"
 import ResizeSession from "lib/sessions/ResizeSession"
 
 describe("Resize Session.", () => {
