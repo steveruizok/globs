056264d56e2222796b27538ebe9b86a7fd47dd2f
Steve Ruiz <steveruizok@gmail.com>
2021-04-19 11:40:22 +0100

    Update code-panel.tsx

--- components/ui/code-panel/code-panel.tsx
+++ components/ui/code-panel/code-panel.tsx
@@ -14,48 +14,47 @@ let code = `// Basic nodes and globs
 
 const nodeA = new Node({
   x: -100,
-  y: 0
-})
+  y: 0,
+});
 
 const nodeB = new Node({
   x: 100,
-  y: 0
-})
+  y: 0,
+});
 
 const glob = new Glob({
   start: nodeA,
   end: nodeB,
-  D: { x: 0, y: 60},
-  Dp: { x: 0, y: 90}
-})
+  D: { x: 0, y: 60 },
+  Dp: { x: 0, y: 90 },
+});
 
 // Something more interesting...
 
+const PI2 = Math.PI * 2,
+  center = { x: 0, y: 0 },
+  radius = 400;
 
-const PI2 = Math.PI * 2, 
-  center = { x: 0, y: 0 }, 
-  radius = 400
-
-let prev
+let prev;
 
 for (let i = 0; i < 21; i++) {
-  const t = i * (PI2 / 20)
+  const t = i * (PI2 / 20);
 
   const node = new Node({
     x: center.x + radius * Math.sin(t),
-    y: center.y + radius * Math.cos(t)
-  })	
+    y: center.y + radius * Math.cos(t),
+  });
 
   if (prev !== undefined) {
     new Glob({
-      start: prev, 
+      start: prev,
       end: node,
       D: center,
-      Dp: center
-    })
-  } 
+      Dp: center,
+    });
+  }
 
-  prev = node
+  prev = node;
 }
 `
 
